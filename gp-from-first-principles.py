7149d8c8d2576f70ead6e24b66b1803375848aec
hwalton <harveywalton.hw@gmail.com>
2023-10-04 13:40:44 +0100

    dataset plotted

--- gp-from-first-principles.py
+++ gp-from-first-principles.py
@@ -2,6 +2,7 @@ from scipy.io import loadmat
 import os
 import csv
 import pandas as pd
+import matplotlib.pyplot as plt
 
 developer = True
 
@@ -10,6 +11,7 @@ def load_data(start = 0, length = 65536):
 
     assert length <= 65536, "Length must be less than or equal to 65536"
 
+    #data collected during MEC326
     df1 = pd.read_csv('datasets/input.csv', header=None)
     df2 = pd.read_csv('datasets/output.csv', header=None)
     df3 = pd.read_csv('datasets/time.csv', header=None)
@@ -27,10 +29,32 @@ def load_data(start = 0, length = 65536):
     print(final_df.head())
     return final_df
 
+def plot_data(data):
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(2, 1, 1)  # 2 rows, 1 column, plot 1
+    plt.scatter(data['time'], data['input'], label='Input', color='blue')
+    plt.xlabel('Time')
+    plt.ylabel('Input')
+    plt.title('Input over Time')
+    plt.legend()
+    plt.grid(True)
+
+    plt.subplot(2, 1, 2)  # 2 rows, 1 column, plot 2
+    plt.scatter(data['time'], data['output'], label='Output', color='green')
+    plt.xlabel('Time')
+    plt.ylabel('Output')
+    plt.title('Output over Time')
+    plt.legend()
+    plt.grid(True)
+
+    plt.tight_layout()  # Adjusts subplot params for better layout
+    plt.show()
 
 def main():
     data = load_data(1000, 1000)
     if developer == True: print(data)
+    plot_data(data)
 
 
 if __name__ == "__main__":
