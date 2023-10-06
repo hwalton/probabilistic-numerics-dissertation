9e9f71a17815f50954eaa79c7ff8a93bab60aa5c
hwalton <harveywalton.hw@gmail.com>
2023-10-06 21:05:03 +0100

    files refactored

---
+++ src/metropolis_hastings.py
@@ -0,0 +1,8 @@
+class MetropolisHastings:
+    def __init__(self,initial_hyperparameters_array, bounds_array, compute_nll):
+        self.initial_hyperparameters_array = initial_hyperparameters_array
+        self.bounds_array = bounds_array
+        self.compute_nll = compute_nll
+
+    def solve(self, n_iter = '10'):
+        return
