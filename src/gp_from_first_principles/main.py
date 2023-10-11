039ea64bfedff49d8eb9ea0506bf874e79e745e5
hwalton <harveywalton.hw@gmail.com>
2023-10-11 02:09:50 +0100

    reverted model to p_se_composite kernels and metropolis_hastings solver for best performance

--- src/gp_from_first_principles/main.py
+++ src/gp_from_first_principles/main.py
@@ -158,13 +158,13 @@ def get_kernel_hyperparameters(kernel_type):
     return initial_hyperparameters, hyperparameter_bounds
 
 def execute_gp_model():
-    sample_start_index = 1000
-    sample_length = 50
-    num_predictions = 24
-    force_input_kernel_type = 'periodic'
-    force_input_solver_type = 'adam'            #'iterative_search', 'metropolis_hastings', 'adam', or 'free_lunch'
-    force_response_kernel_type = 'periodic'
-    force_response_solver_type = 'adam'         #'iterative_search' or 'metropolis_hastings'
+    sample_start_index = 0
+    sample_length = 100
+    num_predictions = 150
+    force_input_kernel_type = 'p_se_composite'
+    force_input_solver_type = 'metropolis_hastings'            #'iterative_search', 'metropolis_hastings', 'adam', or 'free_lunch'
+    force_response_kernel_type = 'p_se_composite'
+    force_response_solver_type = 'metropolis_hastings'         #'iterative_search' or 'metropolis_hastings
     n_iter = 100
     force_input, force_response, time = load_data(sample_start_index,
                                                   sample_length)
