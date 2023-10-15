942b5e877ce3a3ebde10790328cb92764183503e
hwalton <harveywalton.hw@gmail.com>
2023-10-15 21:36:48 +0100

    removed loop in range(1) in compute_nll

--- src/gp_from_first_principles/gp_model.py
+++ src/gp_from_first_principles/gp_model.py
@@ -236,13 +236,12 @@ class GPModel:
             #self.hyperparameters_obj.update(hyperparameters)
             K = self.gp_kernel.compute_kernel(self.X, self.X)
             K += np.repeat(np.array(np.eye(len(self.X)) * 1e-3)[:,:, np.newaxis], self.X.shape[1], axis=2)
-            for i in range(1):
-                L = scipy.linalg.cholesky(K[:, :, i], lower=True)
-                n = len(self.y)
-                one_vector = np.ones(n)
-                y_adj = self.y - self.hyperparameters_obj.dict()['mean_func_c']
-                alpha = scipy.linalg.cho_solve((L, True), y_adj)
-                nll = 0.5 * y_adj.T @ alpha + np.sum(np.log(np.diag(L))) + 0.5 * n * np.log(2 * np.pi)
+            L = scipy.linalg.cholesky(K[:, :, 0], lower=True)
+            n = len(self.y)
+            one_vector = np.ones(n)
+            y_adj = self.y - self.hyperparameters_obj.dict()['mean_func_c']
+            alpha = scipy.linalg.cho_solve((L, True), y_adj)
+            nll = 0.5 * y_adj.T @ alpha + np.sum(np.log(np.diag(L))) + 0.5 * n * np.log(2 * np.pi)
         elif method == 'cholesky_KMeans':
             if type(hyperparameters) == dict:
                 self.hyperparameters_obj.update(hyperparameters)
@@ -502,34 +501,33 @@ class GPModel:
             # K += np.repeat(
             #    np.array(np.eye(len(self.X)) * 1e-3)[:, :, np.newaxis],
             #   self.X.shape[1], axis=2)
-            for i in range(1):
-                n = len(self.y)
-                #    L = scipy.linalg.cholesky(K[:, :, i], lower=True)
-                y_adj = np.squeeze(self.y - self.hyperparameters_obj.dict()[
-                    'mean_func_c'])
-                K_XX_FITC, K_XU, K_UX, K_UU, K_XX, Q_XX, K_UU_inv_K_UX = self.K_XX_FITC()
-                #beta = self.hyperparameters_obj.dict()['noise_level'] ** -1
-                K_sigma_inv = self.K_sigma_inv()
-                #eigs = np.linalg.eigvalsh(K_sigma_inv)
-                #debug_print(f"cov eigenvalues: {eigs}")
-                n = K_XX.shape[0]
-                try:
-                    #var645 = self.hyperparameters_obj.dict()['noise_level'] ** 2 * np.eye(n)
-                    big_lambda = np.diag(np.diag(K_XX-Q_XX)) + self.hyperparameters_obj.dict()['noise_level'] ** 2 * np.eye(n)
-                    #eigs_big_lambda = np.linalg.eigvalsh(big_lambda)
-                    #debug_print(f"big lambda eigenvalues: {eigs_big_lambda}")
-                    in53 = Q_XX + big_lambda + 1E-3 * np.eye(n)
-                    cond_in53 = np.linalg.cond(in53)
-                    debug_print(f"cond_in53: {cond_in53}")
-                    fast_det = self.fast_det(K_XU, K_UU_inv_K_UX, big_lambda)
-                    debug_print(f"fast_det: {fast_det}")
-                    debug_print(f"K_sigma_inv: {K_sigma_inv}")
-                    nll = 0.5 * np.log(fast_det) + 0.5 * y_adj.T @ K_sigma_inv @ y_adj + 0.5 * n * np.log(2 * np.pi)
-                    nll = np.array(-nll)
-                except ValueError:
-                    nll = np.array([10E10])
-                    debug_print(
-                        "Compute NLL failed. Setting nll to a large value.")
+            n = len(self.y)
+            #    L = scipy.linalg.cholesky(K[:, :, 0], lower=True)
+            y_adj = np.squeeze(self.y - self.hyperparameters_obj.dict()[
+                'mean_func_c'])
+            K_XX_FITC, K_XU, K_UX, K_UU, K_XX, Q_XX, K_UU_inv_K_UX = self.K_XX_FITC()
+            #beta = self.hyperparameters_obj.dict()['noise_level'] ** -1
+            K_sigma_inv = self.K_sigma_inv()
+            #eigs = np.linalg.eigvalsh(K_sigma_inv)
+            #debug_print(f"cov eigenvalues: {eigs}")
+            n = K_XX.shape[0]
+            try:
+                #var645 = self.hyperparameters_obj.dict()['noise_level'] ** 2 * np.eye(n)
+                big_lambda = np.diag(np.diag(K_XX-Q_XX)) + self.hyperparameters_obj.dict()['noise_level'] ** 2 * np.eye(n)
+                #eigs_big_lambda = np.linalg.eigvalsh(big_lambda)
+                #debug_print(f"big lambda eigenvalues: {eigs_big_lambda}")
+                in53 = Q_XX + big_lambda + 1E-3 * np.eye(n)
+                cond_in53 = np.linalg.cond(in53)
+                debug_print(f"cond_in53: {cond_in53}")
+                fast_det = self.fast_det(K_XU, K_UU_inv_K_UX, big_lambda)
+                debug_print(f"fast_det: {fast_det}")
+                debug_print(f"K_sigma_inv: {K_sigma_inv}")
+                nll = 0.5 * np.log(fast_det) + 0.5 * y_adj.T @ K_sigma_inv @ y_adj + 0.5 * n * np.log(2 * np.pi)
+                nll = np.array(-nll)
+            except ValueError:
+                nll = np.array([10E10])
+                debug_print(
+                    "Compute NLL failed. Setting nll to a large value.")
         else:
             raise ValueError("Invalid compute_nll method")
         return nll.item()
