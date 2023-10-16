d5edf8e822b1e543bde9314550ccf41c3cdb8157
hwalton <harveywalton.hw@gmail.com>
2023-10-16 22:14:47 +0100

    tidied up utils.py

--- src/gp_from_first_principles/utils.py
+++ src/gp_from_first_principles/utils.py
@@ -1,17 +1,9 @@
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv('../../.env')
-#developer = True
-
 
 env = os.environ.get('ENV')
-
-# def debug_print(text):
-#     if developer:
-#         print(text)
-X = 1
 def debug_print(text):
     if env == 'DEVELOPMENT':
         print(text)
