622c24149b59a3fbf996192589253742b8d5b41e
hwalton <harveywalton.hw@gmail.com>
2023-10-16 01:09:15 +0100

    failing test created for pass

---
+++ src/gp_from_first_principles/test_gp_nll_FITC_18_134.py
@@ -0,0 +1,12 @@
+# test_my_module.py
+import math
+import time as timer
+from gp_model import GPModel
+import unittest
+from utils import debug_print
+from main import format_data, load_data
+import numpy as np
+
+class TestGP_NLL_FITC_18_134(unittest.TestCase):
+    def test_compute(self):
+        pass
