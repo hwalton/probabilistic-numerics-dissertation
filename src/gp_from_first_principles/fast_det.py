01421794483eb017670fee5b69123f2d796cdb04
hwalton <harveywalton.hw@gmail.com>
2023-10-19 00:41:57 +0100

    checkpoint

--- src/gp_from_first_principles/fast_det.py
+++ src/gp_from_first_principles/fast_det.py
@@ -8,6 +8,7 @@ def compute_fast_det(U, V_T, D):
     diag_D = np.diag(D)
     D_inv = np.diag(np.reciprocal(diag_D))  # O(n)
     det_D = np.prod(diag_D)  # O(n)
+    det_D = np.clip(det_D, 1E-6, 1E6)
     det_in = np.eye(m) + V_T @ D_inv @ U
     debug_print(f"min det_d: {np.min(det_D)}, max: {np.max(det_D)}")
     debug_print(f"min det_in: {np.min(det_in)}, max: {np.max(det_in)}")
