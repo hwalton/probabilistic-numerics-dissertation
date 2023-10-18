a5460dc5ea385966c27e183c542287fb8796d1a6
hwalton <harveywalton.hw@gmail.com>
2023-10-18 01:09:35 +0100

    checkpoint

--- src/gp_from_first_principles/main.py
+++ src/gp_from_first_principles/main.py
@@ -69,21 +69,21 @@ def format_data(X):
 
 
 def execute_gp_model():
-    sample_start_index = 10000
+    sample_start_index = 62000
     sample_length = 75
     num_predictions = 40
     force_input_kernel_type = ['squared_exponential', 'p_se_composite', 'white_noise', 'wn_se_composite', 'periodic'][4]
     force_input_solver_type = ['metropolis_hastings', 'iterative_search', 'adam', 'free_lunch'][0]
     force_input_predict_type = ['cholesky', 'FITC'][0]
     force_input_nll_method = ['cholesky', 'FITC_18_134'][1]
-    force_input_n_iter = 50
+    force_input_n_iter = 5
     force_response_kernel_type = ['squared_exponential', 'p_se_composite', 'white_noise', 'wn_se_composite', 'periodic'][4]
     force_response_solver_type = ['metropolis_hastings', 'iterative_search', 'adam', 'free_lunch'][0]
     force_response_predict_type = ['cholesky', 'FITC'][0]
     force_response_nll_method = ['cholesky', 'FITC_18_134'][1]
-    force_response_n_iter = 50
+    force_response_n_iter = 5
 
-    M_one_in = 20
+    M_one_in = 25
 
     force_input, force_response, time = load_data(sample_start_index,
                                                   sample_length)
