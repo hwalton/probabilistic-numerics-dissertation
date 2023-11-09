72134093721eec9b44e795c39dbd8338a58e8dd3
hwalton <harveywalton.hw@gmail.com>
2023-11-09 18:07:02 +0000

    test written for _inverse_upper_triangular

--- src/gp_from_first_principles/test_gp_nll_FITC.py
+++ src/gp_from_first_principles/test_gp_nll_FITC.py
@@ -56,3 +56,54 @@ class TestGP_NLL_FITC(unittest.TestCase):
 
 
         assert np.allclose(result2, correct2, atol = 1E-3, rtol= 1E-3 )
+
+    def test__inverse_upper_triangular(self):
+        obj = GP_NLL_FITC(1, 2, 3, 4, 5, 6)
+
+        # Upper triangular matrix
+        matrix = np.array([
+            [2, 3, 1],
+            [0, 4, 5],
+            [0, 0, 7]
+        ])
+
+        result = obj._inverse_upper_triangular(matrix)
+
+        # The inverse of the upper triangular matrix calculated by hand or another method
+        correct = np.array([
+            [0.5, -3. / 8., 1. / 28.],
+            [0, 0.25, -5. / 28.],
+            [0, 0, 1. / 7.]
+        ])
+
+        correct = np.linalg.inv(matrix)
+
+        debug_print(f"result = {result}")
+        debug_print(f"correct = {correct}")
+        debug_print(f"difference = {result - correct}")
+
+        assert np.allclose(result, correct, rtol=0.01)
+
+        # Another upper triangular matrix with different values
+        matrix2 = np.array([
+            [-1.9883, 2.0, 3.0],
+            [0, -9.4199, 4.0],
+            [0, 0, 1.7969]
+        ])
+
+        result2 = obj._inverse_upper_triangular(matrix2)
+
+        # The inverse of the second upper triangular matrix
+        correct2 = np.array([
+            [-0.5029, 0.1065, -0.1876],
+            [0, -0.1062, -0.2353],
+            [0, 0, 0.5565]
+        ])
+
+        correct2 = np.linalg.inv(matrix2)
+
+        debug_print(f"result2 = {result2}")
+        debug_print(f"correct2 = {correct2}")
+        debug_print(f"difference2 = {result2 - correct2}")
+
+        assert np.allclose(result2, correct2, atol=1E-3, rtol=1E-3)
