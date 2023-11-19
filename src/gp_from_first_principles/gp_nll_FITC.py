7cd5ab9fd1c8e4970176c89dd9be8b0abe41f42c
hwalton <harveywalton.hw@gmail.com>
2023-11-19 19:43:57 +0000

    refactored y_hat to y_hat_adj since it uses y_adj

--- src/gp_from_first_principles/gp_nll_FITC.py
+++ src/gp_from_first_principles/gp_nll_FITC.py
@@ -10,7 +10,7 @@ class GP_NLL_FITC:
         self.K_y_hat_U_R = None
         self.K_y_hat_U = None
         self.K_y_hat_U_T = None
-        self.y_hat = None
+        self.y_hat_adj = None
         self.R = None
         self.K_tilde_Uf = None
         self.big_lambda = None
@@ -52,11 +52,11 @@ class GP_NLL_FITC:
         QR = np.transpose(np.concatenate((self.L_UU,self.K_tilde_Uf), axis=1))
         self.R = np.abs(np.linalg.qr(QR, mode='r'))
 
-        self.y_hat = np.reciprocal(np.diag(self.big_lambda)) * self.y_adj
+        self.y_hat_adj = np.reciprocal(np.diag(self.big_lambda)) * self.y_adj
 
 
 
-        self.K_y_hat_U_T = self.y_hat.T @ self.K_fU
+        self.K_y_hat_U_T = self.y_hat_adj.T @ self.K_fU
 
         self.K_y_hat_U = self.K_y_hat_U_T.T
 
@@ -64,7 +64,7 @@ class GP_NLL_FITC:
 
         self.K_y_hat_U_R = self.K_y_hat_U @ self.R_inv
 
-        term_1_f = 0.5*(np.inner(self.y_adj.T, self.y_hat) - (self.K_y_hat_U_R ** 2).sum())
+        term_1_f = 0.5*(np.inner(self.y_adj.T, self.y_hat_adj) - (self.K_y_hat_U_R ** 2).sum())
 
         term_2_f =  0.5 * np.sum(np.log((np.diag(self.big_lambda)))) \
                     - np.sum(np.log(np.diag(self.L_UU))) \
@@ -85,7 +85,7 @@ class GP_NLL_FITC:
 
     def predict(self, X_test):
         if any(getattr(self, attr) is None for attr in [
-            'K_y_hat_U_R', 'K_y_hat_U', 'K_y_hat_U_T', 'y_hat', 'R', 'K_tilde_Uf',
+            'K_y_hat_U_R', 'K_y_hat_U', 'K_y_hat_U_T', 'y_hat_adj', 'R', 'K_tilde_Uf',
             'big_lambda', 'Q_ff', 'L_UU', 'K_UU', 'K_fU', 'K_ff', 'n_u', 'n_f', 'y_adj']):
             self.compute_nll()
 
@@ -93,7 +93,7 @@ class GP_NLL_FITC:
 
         big_sigma = self.R_inv @ self.R_inv.T
 
-        self.mu = K_star_U @ big_sigma @ self.K_fU.T @ self.y_hat # from quinonero-candela eq. 24b
+        self.mu = K_star_U @ big_sigma @ self.K_fU.T @ self.y_hat_adj # from quinonero-candela eq. 24b
 
         K_star_star = np.squeeze(self.gp_kernel.compute_kernel(X_test, X_test))
 
