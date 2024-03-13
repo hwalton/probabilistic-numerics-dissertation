DFT scaling fixed

src/gp_model.py
@@ -208,6 +208,14 @@ class GPModel:
 
             return self.mu_fourier, self.stdv_fourier
 
+        elif method == 'GP_6':
+            hyp_l, w = self.GP_Mu(X_star)
+
+            self.GP_STDV_6(X_star)
+
+            return self.mu_fourier, self.stdv_fourier
+
+
         elif method == 'DFT':
             self.X = np.asarray(self.X)
             N = len(self.X)
@@ -227,7 +235,7 @@ class GPModel:
 
             self.xi = 2 * np.pi * self.xi # convert from Hz to rad/s
 
-            self.mu_fourier = np.fft.fft(np.squeeze(self.y) * np.hanning(len(np.squeeze(self.y))))
+            self.mu_fourier = np.fft.fft(np.squeeze(self.y) * np.hanning(len(np.squeeze(self.y)))) / (len(np.squeeze(self.y))/2) * 2 * np.pi
             self.stdv_fourier = np.zeros_like(self.mu_fourier)
             return self.mu_fourier, self.stdv_fourier
         elif method == 'set':
@@ -333,7 +341,7 @@ class GPModel:
             for j in range(len(self.X)):
                 for k in range(len(self.X)):
                     innn = A[j][k] * np.exp(-1j * xi_n * (np.squeeze(self.X)[j] - np.squeeze(self.X)[k]))
-                    self.var_fourier[n] -= innn
+                    self.var_fourier[n] += innn    # CHECK!!!: SHOULD THIS BE -= or +=? Maths says -=, but positive and real values are expected, which occur with +=????
             self.var_fourier[n] *= self.gp_kernel.compute_kernel_SE_fourier(xi_n) * np.conj(self.gp_kernel.compute_kernel_SE_fourier(- xi_n))
             debug_k = self.gp_kernel.compute_kernel(-xi_n, 0) / 2 * np.pi
             self.var_fourier[n] += self.gp_kernel.compute_kernel(-xi_n, 0) / 2 * np.pi
@@ -342,21 +350,27 @@ class GPModel:
 
 
     def GP_STDV_5(self, X_star):
+        '''
+        This is a vectorised version of GP_STDV_4, but the += in the line
+        self.var_fourier[n] += innn is replaced with -=. A mathematical reason for this has not been established, but it makes the output positive and real, as expected.
+        '''
         A = np.linalg.inv(np.squeeze(self.K_X_X) + self.hyperparameters_obj.dict()['noise_level'] * np.eye(len(self.X)))
         X_squeezed = np.squeeze(self.X)
         self.stdv_fourier = np.zeros(len(self.xi), dtype=complex)
 
         for n, xi_n in enumerate(self.xi):
-            debug = (X_squeezed[:, None] + X_squeezed)
+            debug = (X_squeezed[:, None] - X_squeezed)
             exponent_matrix = -1j * xi_n * debug
             contributions = A * np.exp(exponent_matrix)
-            self.stdv_fourier[n] = -np.sum(contributions)
+            self.stdv_fourier[n] = np.sum(contributions) # CHECK!!!: SHOULD THIS BE -? Maths says -, but positive and real values are expected, which occur with +????
 
             kernel_fourier_sq = self.gp_kernel.compute_kernel_SE_fourier(xi_n) ** 2
             kernel_fourier_neg = self.gp_kernel.compute_kernel(-xi_n, 0) / 2 * np.pi
 
             self.stdv_fourier[n] *= kernel_fourier_sq
             self.stdv_fourier[n] += kernel_fourier_neg
+
+
 
     def GP_Mu(self, X_star):
         hyp_l = self.hyperparameters_obj.dict()['l']