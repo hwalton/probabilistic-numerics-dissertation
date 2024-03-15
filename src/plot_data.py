923dc6b43ab371e49f292666460e7e39047e5e3e
hwalton <harveywalton.hw@gmail.com>
2024-03-15 11:56:22 +0000

    plot finished

--- src/plot_data.py
+++ src/plot_data.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 def plot_data(plot_df):
-    plt.figure(figsize=(16.5, 11.7))
+    plt.figure(figsize=(23.4, 16.5))
     plt.rcParams.update({'font.size': 16})
 
     arrays = {col: np.array(plot_df[col]) for col in plot_df.columns}
@@ -18,6 +18,29 @@ def plot_data(plot_df):
     GP_FT_mu = arrays['GP_FT_mu']
     GP_FT_stdv = arrays['GP_FT_stdv']
     analytical_FT = arrays['analytical_FT']
+    DFT = arrays['DFT']
+
+    def convert_to_complex(s):
+        return complex(s)
+
+    # Vectorize this function so it can be applied over each element of the array
+    vectorized_convert = np.vectorize(convert_to_complex)
+
+    # Apply the conversion to the entire array
+    GP_FT_mu = vectorized_convert(GP_FT_mu)
+    GP_FT_stdv = vectorized_convert(GP_FT_stdv)
+    analytical_FT = vectorized_convert(analytical_FT)
+    DFT = vectorized_convert(DFT)
+
+
+    # # Split the complex numbers into real and imaginary parts for saving
+    # plot_df['RealPart'] = plot_df['ComplexData'].apply(lambda x: x.real)
+    # plot_df['ImagPart'] = plot_df['ComplexData'].apply(lambda x: x.imag)
+    #
+    # # Save the DataFrame with real and imaginary parts to a CSV file
+    # plot_df[['RealPart', 'ImagPart']].to_csv('complex_data.csv', index=False)
+
+
 
     if force_response_prediction_f.ndim > 1:
         assert 0, "Not yet implemented"
@@ -25,10 +48,10 @@ def plot_data(plot_df):
     else:
         force_response_prediction_diag = np.array(force_response_prediction_f)
 
-
-    plt.subplot(3, 1, 1)  # a rows, b columns, plot c
-    plt.scatter(time, force_response, label='Force Response', color='green')
-    plt.plot(time_test, force_response_prediction_t, label='Prediction Mean', color='red')
+    # plt.subplot(3, 1, 1)  # a rows, b columns, plot c
+    ax1 = plt.subplot2grid((5, 3), (0, 0), colspan=3)
+    ax1.scatter(time, force_response, label='Force Response', color='green')
+    ax1.plot(time_test, force_response_prediction_t, label='Prediction Mean', color='red')
 
     # Assuming prediction_f is the standard deviation
     upper_bound = force_response_prediction_t + force_response_prediction_diag
@@ -37,42 +60,111 @@ def plot_data(plot_df):
     plt.fill_between(np.squeeze(time_test), np.squeeze(lower_bound), np.squeeze(upper_bound), color='blue',
                      alpha=0.2, label='Prediction Std Dev')
 
-    plt.xlabel('Time [s]')
-    plt.ylabel('Acceleration [ms$^{-2}$]')
-    plt.title('Force Response Over Time')
+    ax1.set_xlabel('Time [s]')
+    ax1.set_ylabel('Acceleration [ms$^{-2}$]')
+    ax1.set_title('Force Response Over Time')
 
-    plt.legend()
-    plt.grid(True)
+    ax1.legend()
+    ax1.grid(True)
 
-    plt.tight_layout()
-
-    debug_abs = np.abs(GP_FT_stdv)
     upper_bound_abs = np.abs(GP_FT_mu) + np.abs(GP_FT_stdv)
     lower_bound_abs = np.abs(GP_FT_mu) - np.abs(GP_FT_stdv)
 
 
-    plt.subplot(3, 1, 2)  # a rows, b columns, plot c
-    (plt.scatter(xi, np.abs(GP_FT_mu)))
-    plt.fill_between(np.squeeze(xi), lower_bound_abs, upper_bound_abs, color='blue',
+    ax = plt.subplot2grid((5, 3), (1, 2))
+    ax.scatter(xi, np.abs(GP_FT_mu))
+    ax.fill_between(np.squeeze(xi), lower_bound_abs, upper_bound_abs, color='blue',
                      alpha=0.2, label='Std Dev')
-    plt.xlabel('Freq [Rad/s]')
-    plt.ylabel('Magnitude of Fourier Transform')
+    ax.set_xlabel('Freq [Rad/s]')
+    ax.set_ylabel('Magnitude [ms$^{-2}$]')
+    ax.set_title('GP Fourier Transform')
 
     debug_angle = np.angle(GP_FT_stdv)
 
     upper_bound_angle = np.angle(GP_FT_mu) + np.angle(GP_FT_stdv)
     lower_bound_angle = np.angle(GP_FT_mu) - np.angle(GP_FT_stdv)
 
-    plt.ylim(-0.5 * np.max(np.abs(GP_FT_mu)), 1.5 * np.max(np.abs(GP_FT_mu)))
+    ax.set_ylim(-0.5 * np.max(np.abs(GP_FT_mu)), 1.5 * np.max(np.abs(GP_FT_mu)))
 
 
-    plt.subplot(3, 1, 3)  # a rows, b columns, plot c
-    plt.scatter(xi, np.angle(GP_FT_mu))
-    plt.fill_between(np.squeeze(xi), lower_bound_angle, upper_bound_angle, color='blue',
+    # plt.subplot(3, 1, 3)  # a rows, b columns, plot c
+    ax = plt.subplot2grid((5, 3), (2, 2))
+    ax.scatter(xi, np.angle(GP_FT_mu))
+    ax.fill_between(np.squeeze(xi), lower_bound_angle, upper_bound_angle, color='blue',
                      alpha=0.2, label='Std Dev')
-    plt.xlabel('Freq [Rad/s]')
-    plt.ylabel('Phase of Fourier Transform')
+    ax.set_xlabel('Freq [Rad/s]')
+    ax.set_ylabel('Phase [Rad]')
+
+    # plt.subplot(3, 1, 3)  # a rows, b columns, plot c
+    ax = plt.subplot2grid((5, 3), (3, 2))
+    ax.scatter(xi, np.real(GP_FT_mu))
+    ax.set_xlabel('Freq [Rad/s]')
+    ax.set_ylabel('Real Part [ms$^{-2}$]')
+
+    # plt.subplot(3, 1, 3)  # a rows, b columns, plot c
+    ax = plt.subplot2grid((5, 3), (4, 2))
+    ax.scatter(xi, np.imag(GP_FT_mu))
+    ax.set_xlabel('Freq [Rad/s]')
+    ax.set_ylabel('Imaginary Part [ms$^{-2}$]')
+
+
+    ax = plt.subplot2grid((5, 3), (1, 1))
+    ax.scatter(xi, np.abs(DFT))
+    ax.set_xlabel('Freq [Rad/s]')
+    ax.set_ylabel('Magnitude [ms$^{-2}$]')
+    ax.set_title('Discrete Fourier Transform')
+
+    ax.set_ylim(-0.5 * np.max(np.abs(GP_FT_mu)), 1.5 * np.max(np.abs(GP_FT_mu)))
+
+    # plt.subplot(3, 1, 3)  # a rows, b columns, plot c
+    ax = plt.subplot2grid((5, 3), (2, 1))
+    ax.scatter(xi, np.angle(DFT))
+    ax.set_xlabel('Freq [Rad/s]')
+    ax.set_ylabel('Phase [Rad]')
+
+    # plt.subplot(3, 1, 3)  # a rows, b columns, plot c
+    ax = plt.subplot2grid((5, 3), (3, 1))
+    ax.scatter(xi, np.real(DFT))
+    ax.set_xlabel('Freq [Rad/s]')
+    ax.set_ylabel('Real Part [ms$^{-2}$]')
+
+    # plt.subplot(3, 1, 3)  # a rows, b columns, plot c
+    ax = plt.subplot2grid((5, 3), (4, 1))
+    ax.scatter(xi, np.imag(DFT))
+    ax.set_xlabel('Freq [Rad/s]')
+    ax.set_ylabel('Imaginary Part [ms$^{-2}$]')
+
+
+    ax = plt.subplot2grid((5, 3), (1, 0))
+    ax.scatter(xi, np.abs(analytical_FT))
+    ax.set_xlabel('Freq [Rad/s]')
+    ax.set_ylabel('Magnitude [ms$^{-2}$]')
+    ax.set_title('Analytical Fourier Transform')
+
+    ax.set_ylim(-0.5 * np.max(np.abs(GP_FT_mu)), 1.5 * np.max(np.abs(GP_FT_mu)))
+
+    # plt.subplot(3, 1, 3)  # a rows, b columns, plot c
+    ax = plt.subplot2grid((5, 3), (2, 0))
+    ax.scatter(xi, np.angle(analytical_FT))
+    ax.set_xlabel('Freq [Rad/s]')
+    ax.set_ylabel('Phase [Rad]')
+
+    # plt.subplot(3, 1, 3)  # a rows, b columns, plot c
+    ax = plt.subplot2grid((5, 3), (3, 0))
+    ax.scatter(xi, np.real(analytical_FT))
+    ax.set_xlabel('Freq [Rad/s]')
+    ax.set_ylabel('Real Part [ms$^{-2}$]')
+
+    # plt.subplot(3, 1, 3)  # a rows, b columns, plot c
+    ax = plt.subplot2grid((5, 3), (4, 0))
+    ax.scatter(xi, np.imag(analytical_FT))
+    ax.set_xlabel('Freq [Rad/s]')
+    ax.set_ylabel('Imaginary Part [ms$^{-2}$]')
+
+
+    plt.tight_layout()
     plt.show()
+
 
 
 def main(csv_file):
@@ -80,4 +172,4 @@ def main(csv_file):
     plot_data(plot_df)
 
 if __name__ == "__main__":
-    main("plot_df_2024-03-15_10-35-40")
+    main("plot_df_2024-03-15_11-41-54")
